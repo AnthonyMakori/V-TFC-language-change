b68da58880d61cb2eddd8714f792f119e1b2663f
stanondieki <ondiekistanley21@gmail.com>
2024-08-02 19:07:28 +0300

    updated pages folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e71fa484ec30ab97"; }
+/******/ 		__webpack_require__.h = function() { return "d7b956b4762e5bf2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
